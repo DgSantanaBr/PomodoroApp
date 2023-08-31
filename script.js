6882fcd1e510130614308394e52eb6e9f0f5a891
DgSantanaBR <diiiego.santana2@gmail.com>
2023-08-31 11:39:12 -0300

    inserindo ruido( musica) no toggle

--- script.js
+++ script.js
@@ -5,6 +5,21 @@ const longoBt = document.querySelector('.app__card-button--longo');
 const banner = document.querySelector('.app__image');
 const titulo = document.querySelector('.app__title');
 const botoes = document.querySelectorAll('.app__card-button');
+const musicaFocoInput = document.querySelector('#alternar-musica');
+const musica = new Audio('/sons/luna-rise-part-one.mp3');
+const startPauseBt = document.querySelector('#start-pause');
+
+let tempoDecorridoEmSegundos = 5;
+let intervaloId = null;
+
+musica.loop = true
+musicaFocoInput.addEventListener('change', () => {
+    if (musica.paused) {
+        musica.play()
+    } else {
+        musica.pause()
+    }
+})
 
 focoBt.addEventListener('click', () => {
     alterarContext('foco')
@@ -22,7 +37,7 @@ longoBt.addEventListener('click', () => {
 });
 
 function alterarContext(contexto) {
-    botoes.forEach(function(contexto){
+    botoes.forEach(function (contexto) {
         contexto.classList.remove('active')
     })
     html.setAttribute('data-contexto', contexto);
@@ -33,13 +48,13 @@ function alterarContext(contexto) {
             <strong class="app__title-strong">mergulhe no que importa.</strong>`
             break;
 
-            case "descanso-curto":
-                titulo.innerHTML = `Que tal dar uma respirada?<br>
+        case "descanso-curto":
+            titulo.innerHTML = `Que tal dar uma respirada?<br>
                 <strong class="app__title-strong">Faça uma pausa curta!</strong>`
             break;
 
-            case "descanso-longo":
-                titulo.innerHTML = `Hora de voltar à superfície.<br>
+        case "descanso-longo":
+            titulo.innerHTML = `Hora de voltar à superfície.<br>
                 <strong class="app__title-strong"> Faça uma pausa longa.</strong>`
 
         default:
